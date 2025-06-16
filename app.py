368c5853a60af1725e62a5160f4c9468f18438ff
Ludo2293 <49787830+Ludo2293@users.noreply.github.com>
2025-06-16 07:45:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def html_predict():
 @app.route('/predict_league', methods=("POST", "GET"))
 def html_predict_league():
     def calcul_pred_league(id_league,num_saison):
-        if num_saison==int(chpp.league_fixtures(ht_id=id_league).season):
+        if num_saison==int(chpp.LeagueFixtures(ht_id=id_league).season):
             nb_matchs=min(4*int(chpp.league(ht_id=id_league).current_match_round)-4,56)
         else:
             nb_matchs=56
