eb8185d67d000c405ad4f660d4ae13ee3333e2a1
Ludo2293 <49787830+Ludo2293@users.noreply.github.com>
2025-06-29 11:53:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def html_predict():
 def html_predict_league():
     def calcul_pred_league(id_league,num_saison):
         print(dir(chpp))
-        print(dir(chpp.xml_league_details))
+        print(dir(chpp.xml_league_details(league_level_unit_id=id_league)))
         if num_saison==int(chpp.xml_league_fixtures(id_=id_league).season):
             nb_matchs=min(4*int(chpp.league(id_=id_league).current_match_round)-4,56)
         else:
